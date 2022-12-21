3fc838e06aa4db2ef8030d3a0cbe0fd00195153e
Bart Otten <bart.otten85@gmail.com>
2022-12-21 09:54:35 +0100

    Add product seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Example.Repo.insert!(%Example.Store.Product{name: "Bread", price: 2})
+Example.Repo.insert!(%Example.Store.Product{name: "Milk", price: 1})
+Example.Repo.insert!(%Example.Store.Product{name: "Apples", price: 4})
+Example.Repo.insert!(%Example.Store.Product{name: "Bananas", price: 4})
